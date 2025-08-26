3dcbb014b428da857ec1a82b2e8024ae8628a672
mohammed0705 <djellouli256@gmail.com>
2025-08-26 23:10:50 +0100

    ping in debian

--- app/Http/Controllers/NetworkController.php
+++ app/Http/Controllers/NetworkController.php
@@ -3,25 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Symfony\Component\Process\Process;
 
 class NetworkController extends Controller
 {
     public function ping(Request $request)
     {
-        $ip = $request->query('ip');  // Get the IP address from the query parameter
+        $ip = $request->query('ip');  // Get the IP from query
 
-    if (!$ip) {
-        return response()->json(['reachable' => false], 400);  // Return an error if no IP is provided
-    }
+        if (!$ip) {
+            return response()->json(['reachable' => false], 400); // error if no IP
+        }
 
-    // Windows-compatible ping command (-n 1: 1 ping, -w 1000: 1000ms timeout)
-    $result = exec("ping -n 1 -w 1000 $ip", $output, $status);
+        // Linux ping: -c 1 = send 1 packet, -W 1 = wait 1 second
+        $cmd = escapeshellcmd("ping -c 1 -W 1 $ip");
+        exec($cmd, $output, $status);
 
-    // If the status is 0, the IP is reachable
-    return response()->json([
-        'reachable' => $status === 0,
-    ]);
-
+        // $status === 0 if ping is successful
+        return response()->json([
+            'reachable' => $status === 0,
+        ]);
     }
 }
